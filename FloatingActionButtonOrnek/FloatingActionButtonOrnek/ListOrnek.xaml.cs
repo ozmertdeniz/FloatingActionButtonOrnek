ff691c6a912545139e023c09cdef40d2d3dae2d9
Özmert Deniz <zmert Deniz@DESKTOP-G59QBTQ>
2017-01-11 13:40:08 +0300

    Add project files.

---
+++ FloatingActionButtonOrnek/FloatingActionButtonOrnek/ListOrnek.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace FloatingActionButtonOrnek
+{
+    public partial class ListOrnek : ContentPage
+    {
+        public ListOrnek()
+        {
+            InitializeComponent();          
+
+            var items = new List<string>();
+            for (int i = 0; i < 30; i++)
+            {
+                items.Add(string.Format("Ürün {0}", i));
+            }
+
+            list.ItemsSource = items;
+        }
+
+        void FloatButtonTapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Uyarı", "Floating Buton Tıklandı!", "Tamam");
+        }
+    }
+}
